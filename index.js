4f55bcc91e71855dd9a364cee2e472c1b54a5bd0
Ianm234 <126569965+Ianm234@users.noreply.github.com>
2023-03-26 20:44:24 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // your code here
+  const form = document.getElementById('create-task-form');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+});
+
+});
